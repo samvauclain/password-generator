8a68f511a16cf9e58525357907d29a6a0c90c3de
Sam Place <samvauclain@gmail.com>
2021-10-19 23:38:43 -0700

    trying to see if I can just generate random numbers with specific digit amounts based on inputs, got close

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 // Assignment code here
 
+var randomPW = 0;
 
 function generatePassword() {
   var characters = window.prompt('Choose a length of at least 8 characters and no more than 128 characters');
+  
+  validateInput(characters); 
 
-  // validate prompt text
-  if (characters === "" || characters === null) {
-    window.alert("That character length isn't in the specified range, please try again.");
-    // Call it again
-    return generatePassword()
-  }
+  var rand = "0"
 
-  console.log(characters);
-  return characters;
+  for (let i = 0; i < characters - 2; i++) {
+    rand += "0";
+  } 
+
+  rand = "1" + rand;
+  console.log(rand);
+
+  randomPW = Math.floor(1 + Math.random() * (9 * rand));
+  console.log(randomPW);
+
+  return randomPW;
 
 };
+
+function validateInput(characters) {
+  // validate prompt text
+  if (characters === "" || characters === null) {
+    window.alert("Hmmmm. Looks like nothing was entered, please try again.");
+    return generatePassword();
+  }
+  else if (isNaN(characters)) {
+    window.alert("That character isn't a number. Please enter a number.");
+    return generatePassword();
+  }
+  else if (characters < 8 || characters > 128) {
+    window.alert("That number isn't within the specified range. Please try again.");
+    return generatePassword();
+  }
+  // return true;
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
