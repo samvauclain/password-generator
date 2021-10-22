e617e2b314f4e866a51d0448ab2765fdce7ba171
Sam Place <samvauclain@gmail.com>
2021-10-21 23:12:32 -0700

    found a small chunk of code on stack overflow that I was able to repurpose into my functions, and combine numbers, uppercase, lowercase, & symbols https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
 // Assignment code here
-
-var randomPW = 0;
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '_-+=`"!@#$%^&*[](){}';
+// var randomPW = 0;
 
 function generatePassword() {
-  var characters = window.prompt('Choose a length of at least 8 characters and no more than 128 characters');
-  console.log("characters: ", characters);
+  var characterQuantity = window.prompt('Choose a length of at least 8 characters and no more than 128 characters');
+  console.log("characters: ", characterQuantity);
   
-  validateInput(characters); 
-  var pwNumbers = randNums(characters);
+  var characterQuantity = parseInt(characterQuantity);
+  console.log("It converts now: ", typeof(characterQuantity));
 
-  pwNumbers = rand.join("");
+  validateInput(characterQuantity); 
 
-  console.log("rand: ", pwNumbers);
-  return pwNumbers;
+  var randomPWString = Array.apply(null, Array(characterQuantity)).map(RandomGenerate).join('');
+
+  console.log("rand: ", randomPWString);
+  return randomPWString;
 };
 
-function validateInput(characters) {
+function validateInput(characterQuantity) {
   // validate prompt text
-  if (characters === "" || characters === null) {
+  if (characterQuantity === "" || characterQuantity === null) {
     window.alert("Hmmmm. Looks like nothing was entered, please try again.");
     return generatePassword();
   }
-  else if (isNaN(characters)) {
+  else if (isNaN(characterQuantity)) {
     window.alert("That character isn't a number. Please enter a number.");
     return generatePassword();
   }
-  else if (characters < 8 || characters > 128) {
+  else if (characterQuantity < 8 || characterQuantity > 129) {
     window.alert("That number isn't within the specified range. Please try again.");
     return generatePassword();
   }
   // return true;
 }
 
-function randNums(characters) {
-  rand = [];
-  
-  // loop through as many times as set by the prompt number and add a random number each time to the rand array, parse to sting so we can join the items after. 
-  for (let i = 0; i < characters; i++) {
-    rand[i] = Math.floor(Math.random() * 10);
-    rand[i] = rand[i].toString();
-    console.log(typeof rand[i]);
-    console.log(rand[i]);
-  } 
+function RandomGenerate() {
+    var random = uppercase + lowercase + numbers + symbols;
+    return random[Math.floor(Math.random() * random.length)];
 }
 
+
+// function randNums(characters) {
+//   rand = [];
+  
+  // loop through as many times as set by the prompt number and add a random number each time to the rand array, parse to sting so we can join the items after. 
+  // for (let i = 0; i < characters; i++) {
+  //   rand[i] = Math.floor(Math.random() * 10);
+  //   rand[i] = rand[i].toString();
+    // console.log(typeof rand[i]);
+    // console.log(rand[i]);
+//   } 
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
